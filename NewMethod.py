d8452346d4a3eaaec9e193488812cb8864834f80
asus <836585692@qq.com>
2022-06-08 20:31:56 +0800

    6.8 算法优化 新一算法

---
+++ NewMethod.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import time
+
+def find_circle(path):
+    mask = cv2.imread(path)
+    mask_gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+
+    # 识别轮廓
+    contours, _ = cv2.findContours(mask_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # 计算到轮廓的距离
+    raw_dist = np.empty(mask_gray.shape, dtype=np.float32)
+    for i in range(mask_gray.shape[0]):
+        for j in range(mask_gray.shape[1]):
+            raw_dist[i, j] = cv2.pointPolygonTest(contours[0], (j, i), True)
+
+    # 获取最大值即内接圆半径，中心点坐标
+    minVal, maxVal, _, maxDistPt = cv2.minMaxLoc(raw_dist)
+    abs(minVal)
+    maxVal = abs(maxVal)
+
+    # 画出最大内接圆
+    result = cv2.cvtColor(mask_gray, cv2.COLOR_GRAY2BGR)
+    radius = np.int(maxVal)
+    maxDistPt
+    cv2.circle(result, maxDistPt, radius, (0, 255, 0), 2, 1, 0)
+    cv2.imshow('Maximum inscribed circle', result)
+    cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+    start = time.perf_counter()
+    find_circle('pic/diamond.png')
+    end = time.perf_counter()
+    print("运行耗时", end - start)
