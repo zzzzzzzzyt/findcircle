7bfb115465637d3266d1156e96a8295c2df55f6e
asus <836585692@qq.com>
2022-06-15 22:25:44 +0800

    6.15 更进一步 有更多的一点想法

--- drawContour/drawCircleContour.py
+++ drawContour/drawCircleContour.py
@@ -5,13 +5,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# 直接测试实战的  这还是用普通的还没有加上我们的多进程呢 加上更快
+# 直接测试实战的
 
 def find_circle(path):
     mask = cv2.imread(path)
     mask_gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
     mask_gray[mask_gray < 50] = 0
     mask_gray[mask_gray >= 50] = 255
+    plt.imshow(mask_gray)
+    plt.show()
     # 识别轮廓
     contours, _ = cv2.findContours(mask_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     result = cv2.cvtColor(mask_gray, cv2.COLOR_GRAY2BGR)
@@ -20,13 +22,8 @@ def find_circle(path):
     plt.imshow(result)
     plt.show()
 
-    for contour in contours:
-        # 计算到轮廓的距离
-        perimeter = cv2.arcLength(contour, True)
-        print("周长是", perimeter)
-    # cv2.imshow('Maximum inscribed circle', result)
-    # cv2.waitKey(0)
-    plt.imshow(result)
+    cv2.drawContours(mask, contours, -1, (0, 255, 255), 1)
+    plt.imshow(mask)
     plt.show()
 
 
