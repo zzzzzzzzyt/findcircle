e7cb27bff73299ee2b1e2f1d8e2ae6a0e3bb4a18
asus <836585692@qq.com>
2022-06-15 22:39:15 +0800

    6.15 更进一步 有更多的一点想法

--- drawContour/drawCircleContourMorphology.py
+++ drawContour/drawCircleContourMorphology.py
@@ -5,15 +5,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# 直接测试实战的    这是没有进行形态学变化的
+# 进一步处理 先进行孔洞的膨胀  再进行腐蚀去掉一部分干扰的小孔
 
 def find_circle(path):
     mask = cv2.imread(path)
+
     mask_gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
     mask_gray[mask_gray < 50] = 0
     mask_gray[mask_gray >= 50] = 255
     plt.imshow(mask_gray)
     plt.show()
+
+    # 形态学内核 参数作用
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    # 形态学开运算 参数作用
+    mask_gray = cv2.morphologyEx(mask_gray, cv2., kernel, 1)
+    plt.imshow(mask_gray)
+    plt.show()
+
     # 识别轮廓
     contours, _ = cv2.findContours(mask_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     result = cv2.cvtColor(mask_gray, cv2.COLOR_GRAY2BGR)
