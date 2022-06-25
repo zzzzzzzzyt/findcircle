dc5c2896aa1328c2d928437436e5092c6d86e1ed
asus <836585692@qq.com>
2022-06-25 12:34:48 +0800

    6.25

--- findcirclearea/FindCircleArea.py
+++ findcirclearea/FindCircleArea.py
@@ -10,8 +10,8 @@ import numpy as np
 def find_circle(path):
     mask = cv2.imread(path)
     mask_gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-    mask_gray[mask_gray < 130] = 0
-    mask_gray[mask_gray >= 130] = 255
+    mask_gray[mask_gray < 50] = 0
+    mask_gray[mask_gray >= 50] = 255
     # 识别轮廓
     contours, _ = cv2.findContours(mask_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     result = cv2.cvtColor(mask_gray, cv2.COLOR_GRAY2BGR)
@@ -19,19 +19,22 @@ def find_circle(path):
     np.empty(mask_gray.shape, dtype=np.float32)
     plt.imshow(result)
     plt.show()
-
+    area = 0
     for contour in contours:
         # 计算到轮廓的距离
-        area = cv2.contourArea(contour)
-        print("面积是", area)
+        temp_area = cv2.contourArea(contour)
+        area += temp_area
+        print("面积是", temp_area)
     # cv2.imshow('Maximum inscribed circle', result)
     # cv2.waitKey(0)
     plt.imshow(result)
     plt.show()
+    return area
 
 
 if __name__ == '__main__':
     start = time.perf_counter()
-    find_circle('../pic/true.jpg')
+    area = find_circle('../pic/1.jpg')
     end = time.perf_counter()
     print("运行耗时", end - start)
+    print("轮廓内面积为", area)
